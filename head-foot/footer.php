e5ab2394ebf7fc96713fc86da759571b05ea4704
dinhph18273 <94088116+dinhph18273@users.noreply.github.com>
2021-11-22 23:56:34 +0700

    header-footer. no css

---
+++ head-foot/footer.php
@@ -0,0 +1,32 @@
+<footer class="row">
+    <section>
+        <aside class="box-footer wow zoomIn">
+            <img class="mb-4" src="https://scontent.fhan4-2.fna.fbcdn.net/v/t1.15752-9/256119120_314047153637090_1853539491988398535_n.png?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=1KIKUE89hL0AX9RJv4t&_nc_ht=scontent.fhan4-2.fna&oh=5d05178c33f635487720308a3c6a2d08&oe=61BC7407" alt="">
+        </aside>
+        <aside class="box-footer wow zoomIn">
+            <h3>LOCATION</h3>
+            <ul class="ul">
+                <li><i class="fas fa-map-marker-alt mr-1"></i> CS1:Số 147 Phùng Hưng,Đồng Xuân,Hoàn Kiếm,Hà Nội</li>
+                <li><i class="fas fa-map-marker-alt mr-1"></i> CS2:Số 125 Quán Thánh,Tham Quan,Ba Đình,Hà Nội</li>
+                <li class="pt-2 pb-2"><i class="fas fa-phone fa-flip-horizontal mr-1"></i> 0372.0113.08</li>
+                <li><i class="fas fa-envelope mr-1"></i> dinhlcph18273@fpt.edu.vn</li>
+            </ul>
+        </aside>
+        <aside class="box-footer wow zoomIn">
+            <h3>REVERSATION</h3>
+            <ul class="ul">
+                <li><a href="">0326.669.823 <i class="angle right icon"></i></a></li>
+                <li><a href="">Make a reversation <i class="angle right icon"></i></a></li>
+            </ul>
+        </aside>
+
+        <aside class="box-footer wow zoomIn">
+            <h3>OPENING HOURS</h3>
+            <ul class="ul">
+                <li><a href="">Mon-Fri:8:30am - 10:30pm <i class="angle right icon"></i></a></li>
+                <li><a href="">Sat-Sun 10am - 9:30pm <i class="angle right icon"></i></a></li>
+            </ul>
+        </aside>
+
+    </section>
+</footer>
---
+++ head-foot/header.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="left">
+        <img src="https://scontent.fhan4-2.fna.fbcdn.net/v/t1.15752-9/256119120_314047153637090_1853539491988398535_n.png?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=1KIKUE89hL0AX9RJv4t&_nc_ht=scontent.fhan4-2.fna&oh=5d05178c33f635487720308a3c6a2d08&oe=61BC7407" alt="" width="50%">
+    </div>
+    <div class="right">
+        login
+    </div>
+</header>
