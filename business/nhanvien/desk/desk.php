af6d499831cf677d0c042747f3954560c4b6ade0
mrfriday13th <doantnph18167@fpt.edu.vn>
2021-11-23 08:41:40 +0700

    list-desk(chưa sửa link)

--- business/nhanvien/desk/desk.php
+++ business/nhanvien/desk/desk.php
@@ -1,6 +1,13 @@
 <?php
 require_once './global.php';
 
+
+function loadall_desk(){
+    $sql= "select * from desk where 1 order by desk_id asc limit 0,9";
+            $listdesk=pdo_query($sql);
+            return $listdesk;
+}
+
 function index(){
     echo "Hello";
 }
--- views/desk_ui.php
+++ views/desk_ui.php
@@ -1,1 +1,21 @@
-<h2> Hi Hi Ha Ha Ho Ho</h2>
+
+<div class="list_desk">
+                <?php 
+               foreach($listdesk as $d ){
+                extract($d);
+                $linkdesk="index.php?act=listdesk&id=".$desk_id;
+                if($status=="có"){
+                    echo '<div style="background-color:	#00FF00" class="box-desk">
+                    <a href="'.$linkdesk.'">Bàn số  ' .$desk_id.'</a> &nbsp;--
+                    <a href="#">Tầng  '.$location.' </a>
+                    </div>';
+                }else{
+                    echo '<div class="box-desk">
+                    <a href="'.$linkdesk.'">Bàn số  ' .$desk_id.'</a> &nbsp;--
+                    <a href="#">Tầng  '.$location.' </a>
+                    </div>';}
+                
+            }
+                ?>
+           </div>
+</div> 
