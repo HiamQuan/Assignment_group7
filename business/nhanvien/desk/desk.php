4115accbdbce8c58361bd8e6581c55347ab26be1
HiamBi <ngongquan@gmail.com>
2021-11-24 23:48:04 +0700

    demo

--- business/nhanvien/desk/desk.php
+++ business/nhanvien/desk/desk.php
@@ -4,7 +4,6 @@ function loadall_desk()
 {
     $sql = "select * from desk where 1 order by desk_id asc";
     $listdesk = pdo_query($sql);
-    return $listdesk;
     $VIEW_PAGE =  "./views/nhanvien/desk/desk_ui.php";
     include_once './views/nhanvien/layout/main.php';
 }
