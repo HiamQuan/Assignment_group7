c281afc6de888ca4acfcb81068337392297586a1
mrfriday13th <doantnph18167@fpt.edu.vn>
2021-11-24 17:57:30 +0700

    update màu trạng thái

--- views/nhanvien/desk/desk_ui.php
+++ views/nhanvien/desk/desk_ui.php
@@ -1,8 +1,15 @@
 <div  class="list-desk">
                 <?php 
                foreach($listdesk as $d ):
+                ?><?php
+                if($status=="có"){
+                    echo'<div class="box-desk-2">';
+                }else{
+                    echo'<div class="box-desk">';
+                }
+                
                 ?>
-                   <div class="box-desk">
+                   
                     <a href="<?= STAFF_URL . 'nhanvien/order?id=' . $d['desk_id'] ?>">Bàn số  <?=$d['desk_id']?></a> &nbsp;--
                     <p>Tầng  <?=$d['location']?> </p>
                     </div>
