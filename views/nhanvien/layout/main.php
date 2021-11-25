e0011449f82daf7fdc9eec2e3abe642ec2387f4c
mrfriday13th <doantnph18167@fpt.edu.vn>
2021-11-25 10:00:23 +0700

    sửa css

--- views/nhanvien/layout/main.php
+++ views/nhanvien/layout/main.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css ">
-    <link rel="stylesheet" href="./public/customize/css/bep/dashboard.css">
+    <link rel="stylesheet" href="./public/customize/css/nhanvien/desk.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 </head>
 
