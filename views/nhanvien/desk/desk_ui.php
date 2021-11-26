e57071061cfca3637425d2618aa3efb570ac30f5
HiamBi <ngongquan@gmail.com>
2021-11-27 00:14:00 +0700

    update desk.php

--- views/nhanvien/desk/desk_ui.php
+++ views/nhanvien/desk/desk_ui.php
@@ -1,24 +1,24 @@
 <div class="location">
     <?php
-    foreach ($location as $l):?>
-    <button><a href="<?= STAFF_URL.'nhanvien/desk?location='. $l['location'] ?>">Tầng <?=$l['location']?></a></button>
+    foreach ($location as $l) : ?>
+        <button><a href="<?= STAFF_URL . 'nhanvien/desk?location=' . $l['location'] ?>">Tầng <?= $l['location'] ?></a></button>
     <?php endforeach ?>
 </div>
 
-<div  class="list-desk">
-<?php 
-foreach($dsBan as $d ):
-?>
-               <?php
-                if($d['status']=="có"){
-                    echo'<div class="box-desk-2">';
-                }else{
-                    echo'<div class="box-desk">';
-                }
-                ?>
-                    <a href="<?= STAFF_URL . 'nhanvien/order?id=' . $d['desk_id'] ?>">Bàn số  <?=$d['desk_id']?></a> &nbsp;--
-                    
-                    </div>
-                <?php endforeach ?>
-           </div>
-</div> 
+<div class="list-desk">
+    <?php
+    foreach ($dsBan as $d) :
+    ?>
+        <?php
+        if ($d['status'] == "có") {
+            echo '<div class="box-desk-2">';
+        } else {
+            echo '<div class="box-desk">';
+        }
+        ?>
+        <a href="<?= STAFF_URL . 'nhanvien/order?id=' . $d['desk_id'] ?>">Bàn số <?= $d['desk_id'] ?></a> &nbsp;--
+
+</div>
+<?php endforeach ?>
+</div>
+</div>
