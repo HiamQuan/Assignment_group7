dbc61af838a6b84aa518c1ef3369bcf951915bf3
dinhph18273 <94088116+dinhph18273@users.noreply.github.com>
2021-12-04 17:05:31 +0700

    update

--- views/quanly/account/add-form.php
+++ views/quanly/account/add-form.php
@@ -1,35 +1,34 @@
 <form action="<?= ADMIN_URL . 'user/luu-tao-moi' ?>" method="post" enctype="multipart/form-data">
     <div class="row">
         <div class="col-6 offset-3">
-            <div class="form-group">
-                <label for="">Họ và tên</label>
-                <input type="text" name="name" id="" class="form-control" placeholder="">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="name" id="" placeholder="name@example.com" required>
+                <label for="floatingInput">Họ và tên</label>
             </div>
-            <div class="form-group">
-                <label for="">Tài Khoản</label>
-                <input type="text" name="user_name" id="" class="form-control" placeholder="">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="user_name" id="" placeholder="name@example.com" required>
+                <label for="floatingInput">User name</label>
             </div>
-            <div class="form-group">
-                <label for="">Mật khẩu</label>
-                <input type="password" name="password" id="" class="form-control" placeholder="">
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" name="password" id="" placeholder="name@example.com" required>
+                <label for="floatingInput">Password</label>
             </div>
-            <div class="form-group">
-                <label for="">Số điện thoại</label>
-                <input type="number" name="phone" id="" class="form-control" placeholder="">
+            <div class="form-floating mb-3">
+                <input type="number" class="form-control" name="phone" id="" placeholder="name@example.com" required>
+                <label for="floatingInput">Phone</label>
             </div>
-            <div class="form-group">
-                <label for="">Vai trò</label>
-                <input type="text" name="role" id="" class="form-control" placeholder="">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="role" id="" placeholder="name@example.com" required>
+                <label for="floatingInput">Role</label>
             </div>
-            <div class="form-group">
-                <label for="">Ảnh đại diện</label>
-                <input type="file" name="image" id="" class="form-control" placeholder="">
+            <div class="form-floating">
+                <input type="file" class="form-control" name="image" id="" placeholder="Password" required>
+                <label for="floatingPassword">Ảnh đại diện</label>
             </div>
-            <br>
-            <div class="d-flex justify-content-center">
-                <a href="<?= ADMIN_URL . 'user'?>" class="btn btn-sm btn-danger">Hủy</a>
-                &nbsp;  
-                <button type="submit" class="btn btn-primary btn-sm">Lưu</button>
+            <div class="d-flex justify-content-center mt-3">
+                <a href="<?= ADMIN_URL . 'user' ?>" class="btn btn-lg btn-danger">Hủy</a>
+                &nbsp;
+                <button type="submit" class="btn btn-primary btn-lg">Lưu</button>
             </div>
         </div>
     </div>
--- views/quanly/account/edit-form.php
+++ views/quanly/account/edit-form.php
@@ -1,34 +1,33 @@
 <form action="<?= ADMIN_URL . 'user/luu-sua?id=' . $user['user_id'] ?>" method="post" enctype="multipart/form-data">
     <div class="row">
         <div class="col-6 offset-3">
-            <div class="form-group">
-                <label for="">Họ và tên</label>
-                <input type="text" name="name" value="<?= $user['name'] ?>" id="" class="form-control" placeholder="">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="name" value="<?= $user['name'] ?>" id="" placeholder="name@example.com" required>
+                <label for="floatingInput">Name</label>
             </div>
-            <div class="form-group">
-                <label for="">Tài Khoản</label>
-                <input type="text" name="user_name" value="<?= $user['user_name'] ?>" id="" class="form-control" placeholder="">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="user_name" value="<?= $user['user_name'] ?>" id="" placeholder="name@example.com" required>
+                <label for="floatingInput">User name</label>
             </div>
-            <div class="form-group">
-                <label for="">Số điện thoại</label>
-                <input type="number" name="phone" value="<?= $user['phone'] ?>" id="" class="form-control" placeholder="">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="phone" value="<?= $user['phone'] ?>" id="" placeholder="name@example.com" required>
+                <label for="floatingInput">Phone</label>
             </div>
-            <div class="form-group">
-                <label for="">Vai trò</label>
-                <input type="text" name="role" value="<?= $user['role'] ?>" id="" class="form-control" placeholder="">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="role" value="<?= $user['role'] ?>" id="" placeholder="name@example.com" required>
+                <label for="floatingInput">Role</label>
             </div>
-            <div class="form-group">
+            <div class="form-floating mb-3">
                 <div>
                     <img src="<?= PUBLIC_ASSETS . $user['image'] ?>" width="250">
                 </div>
-                <label for="">Ảnh đại diện</label>
-                <input type="file" name="image" id="" class="form-control" placeholder="">
+                <input type="file" class="form-control" name="image" id="" placeholder="name@example.com">
+                <label for="floatingInput">Ảnh đại diện</label>
             </div>
-            <br>
-            <div class="d-flex justify-content-center">
-                <a href="<?= ADMIN_URL . 'user' ?>" class="btn btn-sm btn-danger">Hủy</a>
+            <div class="d-flex justify-content-center mt-3">
+                <a href="<?= ADMIN_URL . 'user' ?>" class="btn btn-lgq btn-danger">Hủy</a>
                 &nbsp;
-                <button type="submit" class="btn btn-primary btn-sm">Lưu</button>
+                <button type="submit" class="btn btn-primary btn-lg">Lưu</button>
             </div>
         </div>
     </div>
