96461998e035406c6870ae51860320be0f582082
HiamBi <ngongquan@gmail.com>
2021-12-04 21:59:56 +0700

    update css

--- views/nhanvien/order/order-ui.php
+++ views/nhanvien/order/order-ui.php
@@ -1,7 +1,7 @@
 <section class="menu-order">
     <div class="list-food">
         <?php foreach ($list_foods as $x) : ?>
-            <div class="animate__animated animate__fadeInRight list-food-item card">
+            <div class="animate__animated animate__fadeInRight list-food-item card shadow-lg p-3 mb-5 bg-body rounded">
                 <div class="food-image">
                     <img src="<?= IMAGE_URL . 'food/' . $x['image'] ?>" alt="">
                 </div>
