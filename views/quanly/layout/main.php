fcaaa551a13dbd8690b4c5c6ca185c55fbe2bb72
HiamBi <ngongquan@gmail.com>
2021-12-04 16:57:26 +0700

    update

--- views/quanly/layout/main.php
+++ views/quanly/layout/main.php
@@ -11,12 +11,14 @@
 <div class="wrapper">
 
   <!-- Preloader -->
-  <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="<?= ADMIN_ASSETS ?>dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  <div class="preloader flex-column justify-content-center align-items-center dark-mode">
+    <img class="animation__shake" src="<?= IMAGE_URL ?>header/grandrestaurant_logo.png" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
+  <div class="main-header">
   <?php include_once "./views/head_foot/header.php" ?>
+  </div>
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
@@ -26,19 +28,6 @@
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
 
@@ -54,11 +43,6 @@
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.1.0
-    </div>
   </footer>
 
   <!-- Control Sidebar -->
