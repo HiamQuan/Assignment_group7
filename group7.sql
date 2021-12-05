71f7b0709e3cd9ef3387adf9614574287c3d4484
HiamBi <ngongquan@gmail.com>
2021-12-06 02:02:02 +0700

    update

--- group7.sql
+++ group7.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2021 at 03:21 PM
+-- Generation Time: Dec 05, 2021 at 05:10 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -41,14 +41,9 @@ CREATE TABLE `bill` (
 --
 
 INSERT INTO `bill` (`bill_id`, `date`, `amount`, `status`, `desk_id`, `user_id`) VALUES
-(29, '2021-11-29 21:44:31', 192500, 'Đã thanh toán', 1, 1),
-(30, '2021-11-29 21:53:52', 93500, 'Chưa thanh toán', 1, 1),
-(31, '2021-11-29 21:54:19', 115500, 'Chưa thanh toán', 1, 1),
-(32, '2021-11-29 21:56:03', 27500, 'Chưa thanh toán', 1, 1),
-(33, '2021-11-29 21:56:40', 49500, 'Chưa thanh toán', 1, 1),
-(34, '2021-11-29 21:56:58', 27500, 'Chưa thanh toán', 1, 1),
-(35, '2021-11-29 22:01:04', 115500, 'Chưa thanh toán', 1, 1),
-(36, '2021-11-29 22:05:13', 49500, 'Chưa thanh toán', 2, 1);
+(104, '2021-12-04 22:20:40', 66000, 'Chưa thanh toán', 1, 1),
+(105, '2021-12-05 17:03:07', 126500, 'Đã thanh toán', 5, 1),
+(106, '2021-12-05 17:12:31', 33000, 'Chưa thanh toán', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -58,17 +53,18 @@ INSERT INTO `bill` (`bill_id`, `date`, `amount`, `status`, `desk_id`, `user_id`)
 
 CREATE TABLE `category` (
   `category_id` int(11) NOT NULL,
-  `category_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+  `category_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `icon` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `category`
 --
 
-INSERT INTO `category` (`category_id`, `category_name`) VALUES
-(1, 'Món khai vị'),
-(2, 'Món chính'),
-(3, 'Món tráng miệng');
+INSERT INTO `category` (`category_id`, `category_name`, `icon`) VALUES
+(1, 'Món khai vị', NULL),
+(2, 'Món chính', NULL),
+(3, 'Món tráng miệng', NULL);
 
 -- --------------------------------------------------------
 
@@ -89,8 +85,12 @@ CREATE TABLE `desk` (
 --
 
 INSERT INTO `desk` (`desk_id`, `desk_name`, `location`, `status`, `capacity`) VALUES
-(1, '', 1, 'đã đặt', 4),
-(2, '', 1, 'đã đặt', 8);
+(1, '1', 1, 'trống', 4),
+(2, '2', 1, 'trống', 8),
+(4, '3', 1, 'chưa đặt', 4),
+(5, '4', 1, 'trống', 4),
+(6, '1', 2, 'chưa đặt', 4),
+(7, '2', 2, 'chưa đặt', 4);
 
 -- --------------------------------------------------------
 
@@ -99,9 +99,9 @@ INSERT INTO `desk` (`desk_id`, `desk_name`, `location`, `status`, `capacity`) VA
 --
 
 CREATE TABLE `detail_bill` (
-  `quantity` int(11) NOT NULL,
-  `status` int(11) NOT NULL DEFAULT 0,
+  `detail_id` int(11) NOT NULL,
   `time` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'chưa hoàn thành',
   `bill_id` int(11) NOT NULL,
   `food_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -110,23 +110,12 @@ CREATE TABLE `detail_bill` (
 -- Dumping data for table `detail_bill`
 --
 
-INSERT INTO `detail_bill` (`quantity`, `status`, `time`, `bill_id`, `food_id`) VALUES
-(1, 0, '2021-11-29 22:10:50', 29, 2),
-(1, 0, '2021-11-29 22:10:50', 29, 4),
-(2, 0, '2021-11-29 22:10:50', 29, 5),
-(1, 0, '2021-11-29 22:10:50', 29, 6),
-(1, 0, '2021-11-29 22:10:50', 30, 5),
-(1, 0, '2021-11-29 22:10:50', 30, 7),
-(1, 0, '2021-11-29 22:10:50', 31, 2),
-(1, 0, '2021-11-29 22:10:50', 31, 6),
-(1, 0, '2021-11-29 22:10:50', 32, 5),
-(1, 0, '2021-11-29 22:10:50', 33, 4),
-(1, 0, '2021-11-29 22:10:50', 33, 5),
-(1, 0, '2021-11-29 22:10:50', 34, 5),
-(1, 0, '2021-11-29 22:10:50', 35, 6),
-(1, 0, '2021-11-29 22:10:50', 35, 7),
-(1, 0, '2021-11-29 22:10:50', 36, 4),
-(1, 0, '2021-11-29 22:10:50', 36, 5);
+INSERT INTO `detail_bill` (`detail_id`, `time`, `status`, `bill_id`, `food_id`) VALUES
+(23, '2021-12-04 22:20:40', 'chưa hoàn thành', 104, 7),
+(28, '2021-12-05 17:03:08', 'chưa hoàn thành', 105, 5),
+(29, '2021-12-05 17:03:08', 'chưa hoàn thành', 105, 6),
+(30, '2021-12-05 17:03:08', 'chưa hoàn thành', 105, 6),
+(33, '2021-12-05 17:12:31', 'chưa hoàn thành', 106, 3);
 
 -- --------------------------------------------------------
 
@@ -139,7 +128,7 @@ CREATE TABLE `food` (
   `food_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` int(50) NOT NULL,
   `category_id` int(11) NOT NULL,
-  `image` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+  `image` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -164,18 +153,20 @@ INSERT INTO `food` (`food_id`, `food_name`, `price`, `category_id`, `image`) VAL
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `user_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `image` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+  `image` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `user_name`, `password`, `phone`, `role`, `image`) VALUES
-(1, 'Nguyễn Anh Quân', 'songmai96', '0326669823', 'admin', 'food1.jpg');
+INSERT INTO `user` (`user_id`, `user_name`, `name`, `password`, `phone`, `role`, `image`) VALUES
+(1, 'admin01', 'Nguyễn Anh Quân', '123', '0326669823', 'admin', 'chef1.jpg'),
+(2, 'admin02', 'Đinh Hoàng Thảo My', '123', '0325698231', 'admin', 'chef3.jpg');
 
 --
 -- Indexes for dumped tables
@@ -205,7 +196,9 @@ ALTER TABLE `desk`
 -- Indexes for table `detail_bill`
 --
 ALTER TABLE `detail_bill`
-  ADD PRIMARY KEY (`bill_id`,`food_id`);
+  ADD PRIMARY KEY (`detail_id`),
+  ADD KEY `bill_id` (`bill_id`),
+  ADD KEY `food_id` (`food_id`);
 
 --
 -- Indexes for table `food`
@@ -228,7 +221,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `bill_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `bill_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -240,7 +233,13 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `desk`
 --
 ALTER TABLE `desk`
-  MODIFY `desk_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `desk_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `detail_bill`
+--
+ALTER TABLE `detail_bill`
+  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `food`
@@ -252,7 +251,7 @@ ALTER TABLE `food`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
