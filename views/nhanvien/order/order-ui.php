bd4136030783f2af78f56a0108cdd1b60d3abd5e
dinhph18273 <94088116+dinhph18273@users.noreply.github.com>
2021-12-07 03:59:30 +0700

    Update order-ui.php

--- views/nhanvien/order/order-ui.php
+++ views/nhanvien/order/order-ui.php
@@ -30,7 +30,7 @@
         <?php $bill_id = isset($_SESSION['bill-id'][$table_id])? '&bill-id='.$_SESSION['bill-id'][$table_id]: NULL;?>
         <form action="<?= STAFF_URL . 'order?table-id='.$_GET['table-id'].$bill_id ?>" class="d-flex" method="POST">
             <input type="hidden" name="desk_id" value="<?= $_GET['table-id'] ?>">
-            <input class="form-control me-2" name="key" type="search" placeholder="Search" aria-label="Search" autocomplete="off">
+            <input class="form-control me-2" name="key" type="search" placeholder="Search" aria-label="Search" autocomplete="off" required>
             <button class="btn btn-outline-success" name="search" type="submit">Search</button>
         </form>
         <ul>
