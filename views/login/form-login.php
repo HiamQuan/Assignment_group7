e89c00d4291ac4d30006300fc7ee2ac7683781f6
dinhph18273 <94088116+dinhph18273@users.noreply.github.com>
2021-12-17 17:51:48 +0700

    a

--- views/login/form-login.php
+++ views/login/form-login.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng nhập</title>
-    <?php include_once './views/nhanvien/layout/style.php'; 
-    if(isset($_COOKIE['member_login']) && $_COOKIE['member_login'] !=""){
+    <?php include_once './views/nhanvien/layout/style.php';
+    if (isset($_COOKIE['member_login']) && $_COOKIE['member_login'] != "") {
         $a = $_COOKIE['member_login'];
         $sql = "select * from user 
         where user_name =  '$a' ";
@@ -19,18 +19,18 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <h1>Đăng Nhập</h1>
+            <a href="http://localhost:81/Assignment_group7/admin" class="brand-link" style="text-align: center;">
+                <img src="http://localhost:81/Assignment_group7/public/upload/header/grandrestaurant_logo.png" alt="Grand Logo" class="img-fluid" style="opacity:1">
+            </a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
-            <div class="card-body login-card-body" style="background-color: #97969640;">
-                <a href="http://localhost:81/Assignment_group7/admin" class="brand-link" style="text-align: center;">
-                    <img src="http://localhost:81/Assignment_group7/public/upload/header/grandrestaurant_logo.png" alt="Grand Logo" class="img-fluid" style="opacity:1">
-                </a>
+            <div class="card-body login-card-body form">
+                <h1 style="text-align: center;">Login</h1>
                 <p style="color: red;"><?= isset($_GET['err']) ? 'Tài khoản không tồn tại hoặc mật khẩu không chính xác' : ''; ?></p>
                 <form action="<?= BASE_URL . 'login/submit' ?>" method="post">
                     <div class="input-group mb-3">
-                        <input type="text" name="user_name" value="<?= isset($_COOKIE["member_login"]) && $_COOKIE['member_login'] !="" ? $user['user_name'] :  ""; ?>" class="form-control" placeholder="Username" autocomplete="off">
+                        <input type="text" name="user_name" value="<?= isset($_COOKIE["member_login"]) && $_COOKIE['member_login'] != "" ? $user['user_name'] :  ""; ?>" class="form-control" placeholder="Username" autocomplete="off">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user"></span>
@@ -38,7 +38,7 @@
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" name="password" class="form-control" placeholder="Password" value="<?= isset($_COOKIE["member_login"]) && $_COOKIE['member_login'] !="" ? $user['password'] :  ""; ?>">
+                        <input type="password" name="password" class="form-control" placeholder="Password" value="<?= isset($_COOKIE["member_login"]) && $_COOKIE['member_login'] != "" ? $user['password'] :  ""; ?>">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
@@ -48,7 +48,7 @@
                     <div class="row">
                         <div class="col-8">
                             <div class="icheck-primary">
-                                <input name="remember" type="checkbox" id="remember" <?php if(isset($_COOKIE["member_login"])) { ?> checked <?php } ?>>
+                                <input name="remember" type="checkbox" id="remember" <?php if (isset($_COOKIE["member_login"])) { ?> checked <?php } ?>>
                                 <label for="remember">
                                     Remember Me
                                 </label>
